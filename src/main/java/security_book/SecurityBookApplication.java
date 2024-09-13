62d75400c4e387c0cc760cbdece420e13522a10e
login-nigol <vadim.antipov@gmx.de>
2024-09-13 09:59:28 +0200

    created spring security project

---
+++ src/main/java/security_book/SecurityBookApplication.java
@@ -0,0 +1,13 @@
+package security_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security_book/SecurityBookApplicationTests.java
@@ -0,0 +1,13 @@
+package security_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
